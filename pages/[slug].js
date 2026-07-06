43f10ac89ed4b4fe19e55813cd35be8f30c13b59
whileushop.com <surat.digitalstyle@gmail.com>
2026-07-06 22:28:48 +0530

    Remove Google Ads script from [slug].js

--- pages/[slug].js
+++ pages/[slug].js
@@ -122,8 +122,7 @@ export default function DealPage({ deal, structuredData }) {
         }
 
 
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2723752519561361"
-     crossorigin="anonymous"></script>
+
 
 </Head>
 
